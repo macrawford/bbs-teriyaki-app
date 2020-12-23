65be3c3b4d257112b506133674d79232873aa862
Matthew Crawford <matthew.crawford92@gmail.com>
2020-12-23 15:14:57 -0800

    Added google maps api

--- bbsteriyaki/client/Location.js
+++ bbsteriyaki/client/Location.js
@@ -4,17 +4,24 @@ import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 function Location({ navigation }) {
+  // Webview won't work when inside of another component it seems (inside of View currently)
   return (
-    <View style={styles.container}>
-      <View style={styles.input}>
-        <Text style={styles.font}>THIS IS THE LOCATION PAGE</Text>
-      </View>
-      <Button style={styles.button} title="Return to Login" accessibilityLabel="Clicking this button will return to the login screen" color="blue" onPress={() => navigation.navigate('Login')}/>
-      <Button style={styles.button} title="Proceed to Order" accessibilityLabel="Clicking this button will proceed to the order screen" color="blue" onPress={() => navigation.navigate('Order')}/>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <View style={styles.input}>
+    //     <Text style={styles.font}>THIS IS THE LOCATIONz PAGE</Text>
+    //   </View>
+      <WebView
+        originWhitelist={['*']}
+        source={{ html: "<iframe height='600' width='450' frameborder='0' style='border:0' src='https://www.google.com/maps/embed/v1/search?key={ INSERT API KEY HERE AND GET RID OF BRACKETS }&q=bbs+teriyaki+grill+seattle' allowfullscreen><iframe/>"}}
+        // source={{ html: "<iframe src='https://www.google.com/'/>"}}
+      />
+    //   <Button style={styles.button} title="Return to Login" accessibilityLabel="Clicking this button will return to the login screen" color="blue" onPress={() => navigation.navigate('Login')}/>
+    //   <Button style={styles.button} title="Proceed to Order" accessibilityLabel="Clicking this button will proceed to the order screen" color="blue" onPress={() => navigation.navigate('Order')}/>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
@@ -35,6 +42,11 @@ const styles = StyleSheet.create({
   },
   font: {
     fontFamily: 'Helvetica-BoldOblique',
+  },
+  embedMap: {
+    width: 450,
+    height: 250,
+    flex: 1,
   }
 });
 
