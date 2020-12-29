efdef066ecf4496e666ffd69114f7c89a5d04796
Matthew Crawford <matthew.crawford92@gmail.com>
2020-12-29 11:59:45 -0800

    Toggling of menu item selection, conditional style rendering

--- bbsteriyaki/client/Order.js
+++ bbsteriyaki/client/Order.js
@@ -3,24 +3,25 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View, TextInput, Button, CheckBox } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, CheckBox, Pressable } from 'react-native';
 
 function Order({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <View style={styles.input}>
-        <CheckBox
-        // MAY BE DEPRECATED
-        style={styles.container}
-          center
-          title='Click Here'
-        />
+  const [pressed, setState] = React.useState(false)
+    return (
+      <View style={styles.container}>
+        <View style={styles.input}>
+          <Pressable style={styles.container} center onPress={() => setState((!pressed))}>
+            {/* Can make text pressable (no need for Pressable) */}
+            <Text style={ (pressed) ? styles.text : styles.text2}>
+            {`Current status: ${pressed ? 'Selected' : 'Not Selected'}`}
+            </Text>
+          </Pressable>
+        </View>
+        <Button style={styles.button} title="Return to Location" accessibilityLabel="Clicking this button will return to the login screen" color="blue" onPress={() => navigation.navigate('Location')}/>
+        <Button style={styles.button} title="Proceed to Cart" accessibilityLabel="Clicking this button will proceed to the order screen" color="blue" onPress={() => navigation.navigate('Cart')}/>
+        <StatusBar style="auto" />
       </View>
-      <Button style={styles.button} title="Return to Location" accessibilityLabel="Clicking this button will return to the login screen" color="blue" onPress={() => navigation.navigate('Location')}/>
-      <Button style={styles.button} title="Proceed to Cart" accessibilityLabel="Clicking this button will proceed to the order screen" color="blue" onPress={() => navigation.navigate('Cart')}/>
-      <StatusBar style="auto" />
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -29,6 +30,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'red'
+    // Views cannot handle text styling -- only Text can handle font styling
   },
   input: {
     flexDirection: 'row',
@@ -38,8 +41,13 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     width: 125,
   },
-  font: {
+  text: {
+    fontFamily: 'Helvetica',
+    color: 'green'
+  },
+  text2: {
     fontFamily: 'Helvetica-BoldOblique',
+    color: 'red'
   }
 });
 
