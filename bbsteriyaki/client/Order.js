1176453326768ddbea88a2f3d438f1ac4a2df509
Matthew Crawford <matthew.crawford92@gmail.com>
2020-12-29 15:34:52 -0800

    Added functionality to require choosing at least 1 item

--- bbsteriyaki/client/Order.js
+++ bbsteriyaki/client/Order.js
@@ -6,16 +6,48 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View, TextInput, Button, CheckBox, Pressable } from 'react-native';
 
 function Order({ navigation }) {
-  const [pressed, setState] = React.useState(false)
+  const [whiteRice, setWhiteRice] = React.useState(false);
+  const [brownRice, setBrownRice] = React.useState(false);
+  const [yakisoba, setYakisoba] = React.useState(false);
+  const [cabbageSalad, setCabbageSalad] = React.useState(false);
+  const [veggieStirFry, setVeggieStirFry] = React.useState(false);
+  const [broccoli, setBroccoli] = React.useState(false);
+  const [mixedGreenSalad, setMixedGreenSalad] = React.useState(false);
     return (
       <View style={styles.container}>
-        <View style={styles.input}>
-          <Pressable style={styles.container} center onPress={() => setState((!pressed))}>
-            {/* Can make text pressable (no need for Pressable) */}
-            <Text style={ (pressed) ? styles.text : styles.text2}>
-            {`Current status: ${pressed ? 'Selected' : 'Not Selected'}`}
+        <View style={styles.choices} title="Base">
+          <View style={styles.selectionHeader}>
+            <Text>Step 1: Base (Choose Up To 3)</Text>
+            <Text>Please choose 1 and up to 3</Text>
+            <Text>
+              {`${brownRice || whiteRice || yakisoba || cabbageSalad || veggieStirFry || broccoli || mixedGreenSalad ? 'Good to Go' : 'Required'} `}
+              {/* CHANGE GOOD TO GO TO A CHECKMARK LOGO OR SOMETHING COOL?? */}
             </Text>
-          </Pressable>
+          </View>
+          <Text style={ (whiteRice) ? styles.selected : styles.notSelected} onPress={() => setWhiteRice((!whiteRice))}>
+          {`White rice: ${whiteRice ? 'Selected' : 'Not Selected'}`}
+          </Text>
+          <Text style={ (brownRice) ? styles.selected : styles.notSelected} onPress={() => setBrownRice((!brownRice))}>
+          {`Brown rice: ${brownRice ? 'Selected' : 'Not Selected'}`}
+          </Text>
+          <Text style={ (yakisoba) ? styles.selected : styles.notSelected} onPress={() => setYakisoba((!yakisoba))}>
+          {`Yakisoba: ${yakisoba ? 'Selected' : 'Not Selected'}`}
+          </Text>
+          <Text style={ (cabbageSalad) ? styles.selected : styles.notSelected} onPress={() => setCabbageSalad((!cabbageSalad))}>
+          {`Cabbage Salad: ${cabbageSalad ? 'Selected' : 'Not Selected'}`}
+          </Text>
+          <Text style={ (veggieStirFry) ? styles.selected : styles.notSelected} onPress={() => setVeggieStirFry((!veggieStirFry))}>
+          {`Veggie Stir Fry: ${veggieStirFry ? 'Selected' : 'Not Selected'}`}
+          </Text>
+          <Text style={ (broccoli) ? styles.selected : styles.notSelected} onPress={() => setBroccoli((!broccoli))}>
+          {`Broccoli: ${broccoli ? 'Selected' : 'Not Selected'}`}
+          </Text>
+          <Text style={ (mixedGreenSalad) ? styles.selected : styles.notSelected} onPress={() => setMixedGreenSalad((!mixedGreenSalad))}>
+          {`Mixed Green Salad: ${mixedGreenSalad ? 'Selected' : 'Not Selected'}`}
+          </Text>
+        </View>
+        <View style={styles.input} title="Protein">
+
         </View>
         <Button style={styles.button} title="Return to Location" accessibilityLabel="Clicking this button will return to the login screen" color="blue" onPress={() => navigation.navigate('Location')}/>
         <Button style={styles.button} title="Proceed to Cart" accessibilityLabel="Clicking this button will proceed to the order screen" color="blue" onPress={() => navigation.navigate('Cart')}/>
@@ -27,27 +59,28 @@ function Order({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
     color: 'red'
     // Views cannot handle text styling -- only Text can handle font styling
   },
-  input: {
-    flexDirection: 'row',
+  choices: {
+    flexDirection: 'column',
+    alignItems: 'center',
   },
-  inputBox: {
-    borderColor: 'grey',
-    borderWidth: 2,
-    width: 125,
-  },
-  text: {
+  selected: {
     fontFamily: 'Helvetica',
-    color: 'green'
+    color: 'green',
+    fontSize: 18,
   },
-  text2: {
+  notSelected: {
     fontFamily: 'Helvetica-BoldOblique',
-    color: 'red'
+    color: 'red',
+    fontSize: 18,
+  },
+  selectionHeader: {
+    fontSize: 22,
   }
 });
 
